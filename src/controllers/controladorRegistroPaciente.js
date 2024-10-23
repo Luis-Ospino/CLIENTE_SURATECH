65429ce1c8b7eba48091438f5e28fd06796de3fd
Luis-ospino888 <lantonioospino@gmail.com>
2024-10-23 11:15:55 -0500

    creando cliente de SURATECH

---
+++ src/controllers/controladorRegistroPaciente.js
@@ -0,0 +1,14 @@
+// controlador es igual a archivo js para manipular la interfaz gráfica
+// estructura y diseño --> controlar la GUI (1. detectar eventos 2. controlar contenido 3. cambio de diseño)
+// ---> etapa dos conectarse con el api (1. establecer servicios (consumir apis))
+
+/* 
+Objetivos:
+
+1. recoger los datos de un formulario
+
+pasos:
+
+1. Asignamos una id a cada input, select o textarea del formulario
+
+*/
